e952e7202e543d0bc4699f581946b77071cae69e
OhChangUk <o2556@naver.com>
2024-06-09 13:50:04 +0900

    명령어 exit 입력 시 무한루프 탈출

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("자바 텍스트 게시판 시작");
+    Scanner sc = new Scanner(System.in);
+
+    while(true){
+      String cmd = sc.nextLine();
+      if(cmd.equals("exit")){
+        break;
+      }
+
+      System.out.printf("입력받은 명령어 : %s\n", cmd);
+    }
   }
 }
